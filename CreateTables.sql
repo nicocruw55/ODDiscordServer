201c802df1cfc75f19c333f6e59a34b649dc335c
j.heymann <@no>
2024-08-02 12:14:34 +0200

    Updated Create Table to Create View and Renamed Server to ChannelGroup(Makko kauft Drogen auf Telegramm)

--- CreateTables.sql
+++ CreateTables.sql
@@ -55,69 +55,69 @@ CREATE TABLE IF NOT EXISTS ChannelMembers
         foreign key (UserID) references Users (ID)
 );
 
-CREATE TABLE IF NOT EXISTS Server
+CREATE TABLE IF NOT EXISTS ChannelGroups
 (
     ID          int          auto_increment,
     name        varchar(32)  not null,
     primary key (ID)
 );
 
-CREATE TABLE IF NOT EXISTS ServerMembers
+CREATE TABLE IF NOT EXISTS ChannelGroupMembers
 (
     ID          int          auto_increment,
-    ServerID    int          not null,
+    ChannelMembersID    int          not null,
     UserID      int          not null,
     primary key (ID),
-    constraint ServerMembers_ServerID_fk
-        foreign key (ServerID) references Server (ID),
-    constraint ServerMembers_UserID_fk
+    constraint ChannelGroupMembers_ChannelMembersID_fk
+        foreign key (ChannelMembersID) references ChannelGroups (ID),
+    constraint ChannelGroupMembers_UserID_fk
         foreign key (UserID) references Users (ID)
 );
 
-CREATE TABLE IF NOT EXISTS ServerChannels
+CREATE TABLE IF NOT EXISTS ChannelGroupChannels
 (
     ID          int          auto_increment,
-    ServerID    int          not null,
+    ChannelGroupID    int          not null,
     ChannelID   int          not null,
     primary key (ID),
-    constraint ServerChannels_ServerID_fk
-        foreign key (ServerID) references Server (ID),
-    constraint ServerChannels_ChannelID_fk
+    constraint ChannelGroupChannels_ChannelsID_fk
+        foreign key (ChannelGroupID) references ChannelGroups (ID),
+    constraint ChannelGroupChannels_ChannelID_fk
         foreign key (ChannelID) references Channels (ID)
 );
 
 CREATE TABLE IF NOT EXISTS Roles (
-    ID          int          auto_increment,
-    name        varchar(32)  not null,
-    priority     int          not null,
-    primary key (ID)
+                                     ID          int          auto_increment,
+                                     name        varchar(32)  not null,
+                                     priority     int          not null,
+                                     primary key (ID)
 );
 
 CREATE TABLE IF NOT EXISTS RoleMembers
 (
     RoleID      int          not null,
-    ServerMemberID int       not null,
-    primary key (RoleID, ServerMemberID),
+    ChannelGroupMemberID int       not null,
+    primary key (RoleID, ChannelGroupMemberID),
     constraint RoleMembers_RoleID_fk
         foreign key (RoleID) references Roles (ID),
-    constraint RoleMembers_ServerMemberID_fk
-        foreign key (ServerMemberID) references ServerMembers (ID)
+    constraint RoleMembers_ChannelGroupMemberID_fk
+        foreign key (ChannelGroupMemberID) references ChannelGroupMembers (ID)
 );
 
 CREATE TABLE IF NOT EXISTS RequiredRole (
-    RoleID      int          not null,
-    ServerChannelID int       not null,
-    primary key (RoleID, ServerChannelID),
-    constraint RequiredRole_RoleID_fk
-        foreign key (RoleID) references Roles (ID),
-    constraint RequiredRole_ServerChannelID_fk
-        foreign key (ServerChannelID) references ServerChannels (ID)
+                                            RoleID      int          not null,
+                                            ChannelGroupChannelID int       not null,
+                                            primary key (RoleID, ChannelGroupChannelID),
+                                            constraint RequiredRole_RoleID_fk
+                                                foreign key (RoleID) references Roles (ID),
+                                            constraint RequiredRole_ChannelGroupChannelID_fk
+                                                foreign key (ChannelGroupChannelID) references ChannelGroupChannels (ID)
 );
 
 CREATE TABLE IF NOT EXISTS Permissions (
-    ID          int          auto_increment,
-    description varchar(32)  not null,
-    primary key (ID)
+                                           ID          int          auto_increment,
+                                           description varchar(32)  not null,
+                                           primary key (ID)
 );
 
 CREATE TABLE IF NOT EXISTS RolePermissions
@@ -156,17 +156,17 @@ INSERT INTO ChannelMembers (ChannelID, UserID) VALUES (1, 1);
 INSERT INTO ChannelMembers (ChannelID, UserID) VALUES (1, 2);
 INSERT INTO ChannelMembers (ChannelID, UserID) VALUES (2, 3);
 
--- Insert test data for Server
-INSERT INTO Server (name) VALUES ('Main Server');
+-- Insert test data for ChannelGroups
+INSERT INTO ChannelGroups (name) VALUES ('Main ChannelGroup');
 
--- Insert test data for ServerMembers
-INSERT INTO ServerMembers (ServerID, UserID) VALUES (1, 1);
-INSERT INTO ServerMembers (ServerID, UserID) VALUES (1, 2);
-INSERT INTO ServerMembers (ServerID, UserID) VALUES (1, 3);
+-- Insert test data for ChannelGroupMembers
+INSERT INTO ChannelGroupMembers (ChannelMembersID, UserID) VALUES (1, 1);
+INSERT INTO ChannelGroupMembers (ChannelMembersID, UserID) VALUES (1, 2);
+INSERT INTO ChannelGroupMembers (ChannelMembersID, UserID) VALUES (1, 3);
 
--- Insert test data for ServerChannels
-INSERT INTO ServerChannels (ServerID, ChannelID) VALUES (1, 1);
-INSERT INTO ServerChannels (ServerID, ChannelID) VALUES (1, 2);
+-- Insert test data for ChannelGroupChannels
+INSERT INTO ChannelGroupChannels (ChannelGroupID, ChannelID) VALUES (1, 1);
+INSERT INTO ChannelGroupChannels (ChannelGroupID, ChannelID) VALUES (1, 2);
 
 -- Insert test data for Roles
 INSERT INTO Roles (name, priority) VALUES ('Admin', 1);
@@ -174,13 +174,13 @@ INSERT INTO Roles (name, priority) VALUES ('Moderator', 2);
 INSERT INTO Roles (name, priority) VALUES ('Member', 3);
 
 -- Insert test data for RoleMembers
-INSERT INTO RoleMembers (RoleID, ServerMemberID) VALUES (1, 1);
-INSERT INTO RoleMembers (RoleID, ServerMemberID) VALUES (2, 2);
-INSERT INTO RoleMembers (RoleID, ServerMemberID) VALUES (3, 3);
+INSERT INTO RoleMembers (RoleID, ChannelGroupMemberID) VALUES (1, 1);
+INSERT INTO RoleMembers (RoleID, ChannelGroupMemberID) VALUES (2, 2);
+INSERT INTO RoleMembers (RoleID, ChannelGroupMemberID) VALUES (3, 3);
 
 -- Insert test data for RequiredRole
-INSERT INTO RequiredRole (RoleID, ServerChannelID) VALUES (1, 1);
-INSERT INTO RequiredRole (RoleID, ServerChannelID) VALUES (2, 2);
+INSERT INTO RequiredRole (RoleID, ChannelGroupChannelID) VALUES (1, 1);
+INSERT INTO RequiredRole (RoleID, ChannelGroupChannelID) VALUES (2, 2);
 
 -- Insert test data for Permissions
 INSERT INTO Permissions (description) VALUES ('Read');
@@ -192,3 +192,15 @@ INSERT INTO RolePermissions (RoleID, PermissionID) VALUES (1, 1);
 INSERT INTO RolePermissions (RoleID, PermissionID) VALUES (1, 2);
 INSERT INTO RolePermissions (RoleID, PermissionID) VALUES (2, 1);
 INSERT INTO RolePermissions (RoleID, PermissionID) VALUES (3, 1);
+
+
+
+create definer = DAO@`%` view MessagesWithUsername as
+select `m`.`ID`         AS `ID`,
+       `m`.`timestamp`  AS `timestamp`,
+       `m`.`channel_ID` AS `channel_ID`,
+       `m`.`user_ID`    AS `user_ID`,
+       `u`.`name`       AS `name`,
+       `m`.`content`    AS `content`
+from (`ODDiscord`.`Messages` `m` join `ODDiscord`.`Users` `u` on (`u`.`ID` = `m`.`user_ID`))
+
